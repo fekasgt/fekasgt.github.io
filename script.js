024fc6bc98e556750e3c6148bc0a66c7dfa8c861
fekasgt <fekasgt@users.noreply.github.com>
2021-07-29 09:15:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const colorBtn = document.querySelector('.colorBtn');
-const bodyBcg = document.querySelector('body');
+// const colorBtn = document.querySelector('.colorBtn');
+// const bodyBcg = document.querySelector('body');
 
-const colors = ['yellow','red','green','#6b4556'];
+// const colors = ['yellow','red','green','#6b4556'];
 
-colorBtn.addEventListener('click',changeColor);
+// colorBtn.addEventListener('click',changeColor);
 
-function changeColor() {
-	// bodyBcg.style.backgroundColor = colors[2];
-	let random = Math.floor(Math.random()*colors.length);
-	bodyBcg.style.backgroundColor = colors[random];
-}
+// function changeColor() {
+// 	// bodyBcg.style.backgroundColor = colors[2];
+// 	let random = Math.floor(Math.random()*colors.length);
+// 	bodyBcg.style.backgroundColor = colors[random];
+// }
